57b88ab932ba1d35602b18aec219010ba130615a
MichalLekacz <87401492+MichalLekacz@users.noreply.github.com>
2023-11-07 22:17:10 +0000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}", "node_modules/preline/dist/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: ["preline/plugin"],
+}
+
